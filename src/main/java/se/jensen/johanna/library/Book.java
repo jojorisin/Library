139c43dd9450a86f5dc0479e62126c2aa3fb1cf6
johannajonsson <jojo.risin@outlook.com>
2025-09-22 11:43:30 +0200

    string title

--- src/main/java/se/jensen/johanna/library/Book.java
+++ src/main/java/se/jensen/johanna/library/Book.java
@@ -1,6 +1,12 @@
 package se.jensen.johanna.library;
 
 public class Book {
+    private Author author;
     private String title;
+    private String genre;
+    private String isbn;
+    private boolean borrowed;
+    private Loan loan;
+
 
 }
